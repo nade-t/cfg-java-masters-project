072a5cd78c599668f9dc4ab9616c30a9c6d68afb
Jess-Strange <jessiestrange@hotmail.com>
2025-06-03 22:06:53 +0100

    Updating the attributes to match the books table in SQL.

--- src/main/java/com/cfgmasters/project/model/Book.java
+++ src/main/java/com/cfgmasters/project/model/Book.java
@@ -8,27 +8,29 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+import java.math.BigDecimal;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Table(name = "table-name") // change this once we have the DB code
+@Table(name = "books")
 @Entity
 @Data
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "book_id") // change once we have DB code
+    @Column(name = "book_id")
     private Long id;
 
-    @Column(name = "title", length = 50, nullable = false) // change once we have DB code
+    @Column(name = "title", length = 50, nullable = false)
     private String title;
 
-    @Column(name = "author") // change once we have DB code
+    @Column(name = "author", length = 30, nullable = false)
     private String author;
 
-    @Column(name = "price") // change once we have DB code
-    private double price;
+    @Column(name = "price", precision = 4,scale = 2, nullable = false)
+    private BigDecimal price;
 
-    @Column(name = "copies_available") // change once we have DB code
+    @Column(name = "copies_available", length = 3)
     private int copiesAvailable;
 }
