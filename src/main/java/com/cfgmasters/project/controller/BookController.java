e50235ad64bcd6aa97c9a124fdba85e2e3ee23de
Violeta <Violeta_DePereda@swissre.com>
2025-06-04 09:03:42 +0200

    create POST/add book endpoint

---
+++ src/main/java/com/cfgmasters/project/controller/BookController.java
@@ -0,0 +1,27 @@
+package com.cfgmasters.project.controller;
+
+
+import com.cfgmasters.project.model.Book;
+import com.cfgmasters.project.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+@RequestMapping("/books")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping
+    public ResponseEntity<Book> addBook(@Valid @RequestBody Book book) {
+        log.info("A new book has been added to the bookstore.");
+        return ResponseEntity.ok(bookService.addbook(book));
+    }
+}
