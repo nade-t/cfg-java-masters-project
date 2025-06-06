fa566b2e15430c7139ff312c43ef903baad246df
Radhika Nanavati <Drrads30@gmail.com>
2025-06-06 16:38:55 +0100

    Addition of controller test file

---
+++ src/test/java/com/cfgmasters/project/controller/BookControllerTest.java
@@ -0,0 +1,21 @@
+package com.cfgmasters.project.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+
+class BookControllerTest {
+
+    @Test
+    void whenAddBook_withValidBook_thenBookAdded() {
+
+    }
+
+    @Test
+    void whenGetAllBooks_thenReturnsListOfBooks() {
+
+    }
+}
