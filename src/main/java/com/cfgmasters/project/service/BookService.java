76d76eb03ce3f7811fd108d3152ba21da38d7881
Nade Townsend <nade.townsend@gmail.com>
2025-06-06 18:08:28 +0100

    sales method added to BookService

--- src/main/java/com/cfgmasters/project/service/BookService.java
+++ src/main/java/com/cfgmasters/project/service/BookService.java
@@ -5,6 +5,7 @@ import com.cfgmasters.project.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 import java.util.List;
 
 @Slf4j
@@ -26,5 +27,38 @@ public class BookService {
 
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
+    }
+
+    public Book sellBook(Long id, int salesQuantity) {
+        try {
+            if (salesQuantity <= 0) {
+                throw new IllegalArgumentException("Sales quantity must be greater than 0");
+            }
+
+            Book book = bookRepository.findById(id)
+                    .orElseThrow(() -> new RuntimeException("Book with id: " + id + " not found"));
+
+            int availableCopies = book.getCopiesAvailable();
+
+            if (salesQuantity > availableCopies) {
+                throw new IllegalArgumentException("Not enough copies in stock. Number available for purchase: "
+                        + availableCopies);
+            }
+
+            book.setCopiesAvailable(availableCopies - salesQuantity);
+            Book updatedBookStock = bookRepository.save(book);
+
+            log.info("{} copy(ies) of book id {} sold. Remaining stock: {}", salesQuantity, id,
+                    updatedBookStock.getCopiesAvailable());
+
+            return updatedBookStock;
+
+        } catch (IllegalArgumentException e) {
+            log.error("Unable to process purchase: {}", e.getMessage());
+            throw e;
+        } catch (Exception e) {
+            log.error("Unable to process purchase: {}", e.getMessage());
+            throw new RuntimeException("Unable to process purchase: " + e.getMessage());
+        }
     }
 }
