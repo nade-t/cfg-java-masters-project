be21236a15cb4ec5cd45739281d519faa8274747
Radhika Nanavati <Drrads30@gmail.com>
2025-06-06 21:37:38 +0100

    Edited formatting

--- src/test/java/com/cfgmasters/project/controller/BookControllerTest.java
+++ src/test/java/com/cfgmasters/project/controller/BookControllerTest.java
@@ -26,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class BookControllerTest {
     @Autowired
     private MockMvc mvc;
+
     @MockitoBean
     private BookService bookService;
 
