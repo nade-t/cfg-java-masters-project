d684dd20d1fc2f8489775a79efeefb0cf8c9b53b
Jess-Strange <jessiestrange@hotmail.com>
2025-06-08 17:35:46 +0100

    Creating BookServiceTest class.

---
+++ src/test/java/com/cfgmasters/project/service/BookServiceTest.java
@@ -0,0 +1,12 @@
+package com.cfgmasters.project.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookServiceTest {
+
+    @Test
+    void getAllBooks() {
+    }
+}
